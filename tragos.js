4764a092ec4b0f79000eff5336ebf033ccf7853c
Natalia Reggiani <nreggiani77@gmail.com>
2022-06-20 17:55:56 -0300

    mas datos

--- tragos.js
+++ tragos.js
@@ -2,7 +2,7 @@ const cocktails =[
 
     {
         nombre :'Old Fashioned',
-        imagen:'https://i0.wp.com/blog.scoolinary.com/wp-content/uploads/2021/06/old_fashioned.jpg?w=1000&ssl=1',
+        imagen:'https://www.clarin.com/img/2020/09/08/I_AzjvVPc_340x340__1.jpg',
         receta:`Refrescar en un mixin glass con abundante hielo 60 ml de Bourbon, 7 ml de almibar simple y 2 dash de Bitter Angostura.
         Servir y perfumar con los aceites de la piel de naranja.`,
         ingredientes:['bourbon', 'almibar', 'bitter angostura', 'naranja']
@@ -48,7 +48,7 @@ const cocktails =[
         imagen:'https://imag.bonviveur.com/foto-de-portada-del-manhattan.webp',
         receta:`Colocar 50ml de Bourbon, 25 ml de Martini Rosso y 2 dash de Bitter Angostura en un
         mixin glass y refrescar por aproximadamente 30 segundos. Colar sobre una copa de 
-        cocktail previamente refrescada. Perfumar y decorar con piel de naranja.`,
+        cocktail previamente refrescada. Perfumar y decorar con piel de naranja y una cereza.`,
         ingredientes:['bourbon', 'martini rosso', 'bitter angostura']
     },
 
@@ -112,4 +112,50 @@ const cocktails =[
         con abundante hielo y batir enérgicamente. Colar y servir perfumando con limón deshidratado.`,
         ingredientes:['cognac', 'cointreau', 'jugo de limon']
     },
+
+    {
+        nombre:'White Russian', 
+        imagen:'https://www.eatclub.tv/wp-content/uploads/sites/4/2021/12/whiterussianec-1200x675.jpg',
+        receta:`Colocar 30ml de Vodka, 30ml de Licor de Café y 30ml de Crema de leche en una coctelera 
+        y batir enérgicamente con abundante hielo. Colar sobre vaso de trago corto
+        y agregar un cubo grande de hielo.`,
+        ingredientes:['vodka', 'licor de cafe', 'crema']
+    },
+    
+    {
+        nombre:'Mojito', 
+        imagen:'https://staticfanpage.akamaized.net/wp-content/uploads/sites/22/2021/07/mojito-1200x675.jpg',
+        receta:`Agregar 3 penachos de menta, 30ml de almíbar y 30 ml de jugo de lima y pisar 
+        suavemente en el vaso. Agregar 60ml de Ron, completar con hielo picado y revolver. Completar 
+        con soda. Decorar con menta y una rodaja de lima.`,
+        ingredientes:['ron', 'almibar', 'jugo de lima', 'soda', 'menta']
+    },
+
+    {
+        nombre:'Daiquiri', 
+        imagen:'https://www.tucocteleria.com/wp-content/uploads/2018/04/Daiquiri.jpg',
+        receta:`Colocar 60ml de Ron, 30ml de jugo de lima y 2ctas de azúcar en una ccotelera 
+        con abundante hielo. Batir enérgicamente, colar y servir en copa cocktail.`,
+        ingredientes:['ron', 'jugo de lima', 'azucar']
+    },
+
+    {
+        nombre:'Whisky Sour', 
+        imagen:'https://cocinaalchile.com/wp-content/uploads/2020/03/whisky.jpg',
+        receta:`Colocar 45ml de Bourbon, 30ml de jugo de limón y 2ctas de azúcar en una coctelera con hielo
+        y agitar durante 10 segundos. Colar y servir en vaso corto con hielo. Decorar con cáscara 
+        de naranja.`,
+        ingredientes:['bourbon', 'jugo de limon', 'azucar']
+    },
+
+    {
+        nombre:'Black Russian', 
+        imagen:'https://okdiario.com/img/recetas/2017/08/14/coctel-black-russian.jpg',
+        receta:`Colocar en vaso corto 3 cubos de hielo, 4ml de Licor de café y 8ml de Vodka. 
+        Mezclar.`,
+        ingredientes:['vodka', 'licor de cafe']
+    },
+
+    
+
 ]
